c8362fcf44fd3900d5f5dcf6db88b7c3074ccbcc
Haifa <hnaimm0@gmail.com>
2024-07-01 16:35:23 +0300

    add delete book api

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const books = [
+let books = [
   {
     id: 1,
     title: 'The Great Gatsby',
@@ -20,9 +20,25 @@ const books = [
   },
 ];
 
+// GET list of books
 app.get('/api/books', (req, res) => {
   res.json(books);
 });
+
+// DELETE book by Id
+app.delete('api/books/:id' , (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBooks =  books.filter((book) => book.id!==parseInt(id));
+    if (updatedBooks.length === books.length) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    books = updatedBooks
+    res.status(200).json({ message: 'Book deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+})
 
 const server = app.listen(3030, () => {
   console.log('Server is listening on port 3030');
