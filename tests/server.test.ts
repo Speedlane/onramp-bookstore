4b2ea011c9c9df02121f95b26903b59e7bbbde04
Haifa <hnaimm0@gmail.com>
2024-07-01 16:35:40 +0300

    add tests for delete book api

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -22,3 +22,22 @@ describe('Get /api/books', () => {
     });
   });
 });
+
+
+describe('Delete /api/books/:id', () => {
+  it('should delete a book if found', async () => {
+    const response = await request(app).delete(`/api/books/1`);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Book deleted successfully');
+
+    const newBooksList = await request(app).get('/api/books');
+    expect(newBooksList.status).toBe(200);
+    expect(newBooksList.body.find((book: Book) => book.id===1)).toBe(undefined);
+  });
+
+  it('should return 404 response if book not found', async () => {
+    const response = await request(app).delete(`/api/books/0`);
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('Book not found');
+  })
+});
